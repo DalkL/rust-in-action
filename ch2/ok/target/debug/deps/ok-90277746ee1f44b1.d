d86d74769d47c620f2e9d8c8bd46b69eda8aad52
dalkl <dalkiriadis.lukas@gmail.com>
2025-11-23 15:02:36 -0500

    Created first project in ch2.

---
+++ ch2/ok/target/debug/deps/ok-90277746ee1f44b1.d
@@ -0,0 +1,5 @@
+/home/dalkl/rust-in-action/ch2/ok/target/debug/deps/ok-90277746ee1f44b1.d: src/main.rs
+
+/home/dalkl/rust-in-action/ch2/ok/target/debug/deps/libok-90277746ee1f44b1.rmeta: src/main.rs
+
+src/main.rs:
---
+++ ch2/ok/target/debug/ok.d
@@ -0,0 +1,1 @@
+/home/dalkl/rust-in-action/ch2/ok/target/debug/ok: /home/dalkl/rust-in-action/ch2/ok/src/main.rs
